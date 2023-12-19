c2ea6956dc80ceee08679c479734a6c5c6c19b5c
Adam L <319494+we11adam@users.noreply.github.com>
2023-12-19 13:24:18 +0800

    feat: default season number 01

--- main.go
+++ main.go
@@ -20,36 +20,34 @@ func main() {
 	content, err := os.ReadFile(matchfile)
 
 	if err == nil {
-		fmt.Printf(".plexmatch file already exists:\n%s ", string(content))
+		fmt.Printf("The .plexmatch file already exists:\n%s", string(content))
 		os.Exit(1)
 	}
 
-	fmt.Printf("Please input IMDb id (e.g. tt0903747): ")
 	reader := bufio.NewReader(os.Stdin)
-	line, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	matched, err := regexp.MatchString(`tt\d+`, line)
+
+	fmt.Printf("Please input IMDb id (e.g. tt0903747): ")
+	line, _ := reader.ReadString('\n')
+	line = strings.TrimSpace(line)
+	matched, _ := regexp.MatchString(`tt\d+`, line)
 	if !matched {
 		fmt.Printf("Invalid IMDb id: %s\n", line)
 		os.Exit(1)
 	}
+	imdbid := line
 
-	imdbid := strings.TrimSpace(line)
-
-	fmt.Printf("please input season number (e.g. 02): ")
-	line, err = reader.ReadString('\n')
-
-	matched, err = regexp.MatchString(`\d+`, line)
-
+	fmt.Printf("Please input season number (e.g. 02, defaults to 01): ")
+	line, _ = reader.ReadString('\n')
+	line = strings.TrimSpace(line)
+	if line == "" {
+		line = "01"
+	}
+	matched, _ = regexp.MatchString(`\d+`, line)
 	if !matched {
 		fmt.Printf("Invalid season number: %s\n", line)
 		os.Exit(1)
 	}
-
-	season := strings.TrimSpace(line)
+	season := line
 
 	file, err := os.Create(matchfile)
 	if err != nil {
@@ -62,7 +60,7 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
-	} else {
-		fmt.Printf("Successfully wrote to file the following content to %s:\n%s", matchfile, data)
 	}
+
+	fmt.Printf("Successfully wrote .plexmatch with the following content:\n%s", data)
 }
